e4b14dfe391778843bd232c9539ad1bd9b6c2b8e
Soybeany <soybeany@qq.com>
2022-03-30 10:25:55 +0800

    升级依赖项

--- FileCacheCore/src/main/java/com/soybeany/system/cache/core/exception/FileDownloadException.java
+++ FileCacheCore/src/main/java/com/soybeany/system/cache/core/exception/FileDownloadException.java
@@ -1,12 +1,13 @@
 package com.soybeany.system.cache.core.exception;
 
+import com.soybeany.exception.BdException;
 import lombok.Getter;
 
 /**
  * @author Soybeany
  * @date 2022/2/24
  */
-public class FileDownloadException extends Exception {
+public class FileDownloadException extends BdException {
 
     @Getter
     private final int code;
